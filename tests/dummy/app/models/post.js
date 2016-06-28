725732817fdac295c4a37cbe3508d7e9dd03ef31
Edward Faulkner <ef@alum.mit.edu>
2016-06-28 10:24:48 -0400

    simplifying demo

--- tests/dummy/app/models/post.js
+++ tests/dummy/app/models/post.js
@@ -1,7 +1,8 @@
-import DrupalNode from 'cardstack-cms/models/drupal-node';
 import attr from 'ember-data/attr';
+import Model from 'ember-data/model';
 
-export default DrupalNode.extend({
+export default Model.extend({
   body: attr('mobiledoc'),
-  title: attr('string')
+  title: attr('string'),
+  featured: attr('boolean', { editorial: true })
 });
